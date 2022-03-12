21fd74b6490bfaae7db3eff154a36796b0cc8294
Gabriel Abraham. Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-03-12 18:56:12 +0100

    comments added to code for better understanding of what is happing in the app

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 function newItem(){
-
+//function dleclearation which eable the newItem when activated with click on the "Add" button (id button)
   let li = $('<li></li>');
   let inputValue = $('#input').val();
   li.append(inputValue);
@@ -10,11 +10,11 @@ function newItem(){
   } else {
     $('#list').append(li);
   }
-
+ // Function that strike through items when user clicks on it
   function crossOut() {
 		li.toggleClass("strike");
 	}
-
+//eventListner (li.on) double click will call the function crossOut and change back (toggle) which is the li class,
 	li.on("dblclick", function crossOut() {
 		li.toggleClass("strike");
 	});
@@ -23,9 +23,11 @@ function newItem(){
   crossOutButton.append(document.createTextNode('X'));
   li.append(crossOutButton);
 
+// A functiion that delete items from the list when clicking on the deleteButton
   crossOutButton.on("click", deleteListItem);
   function deleteListItem(){
 	li.addClass("delete")
 	}
+  // Statement that Change the given order of items in the List
    $('#list').sortable();
 }
